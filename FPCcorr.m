ef3b2774b283ac5eff58feef47bfd0db13d2aac1
Amir Shamaei <56726574+amirshamaei@users.noreply.github.com>
2021-11-17 13:42:47 +0100

    Add files via upload

---
+++ FPCcorr.m
@@ -0,0 +1,76 @@
+% Written by amir shamaei amirshamaei@isibrno.cz 2021
+% Algorithm is based on "Automatic frequency and phase alignment of in vivo
+% J-difference-edited MR spectra by frequency domain correlation" paper
+% Authors find the maximum using manual grid search, but here the
+% problem is converted to a non-linear minimiazation problem which can be
+% solved by direct search or newton family methods,
+
+function rslt = FPCcorr(fid, signals, method, range, dwelltime, visus )
+
+global fid
+% fid = refrence;
+global t
+global signals
+% signals = inSignals;
+global range
+% range = ranges;
+global shifted
+global visu
+visu = visus
+global k
+k = 1
+t=0:dwelltime:(length(fid)-1)*dwelltime;
+parsFit=[0,0];
+if visu == true
+    if method == "fminunc"
+        options = optimoptions('fminunc','PlotFcns',@optimplotfval);
+    elseif method == "fminsearch"
+        options = optimset('PlotFcns',@optimplotfval);
+    end
+elseif visu == false
+    if method == "fminunc"
+        options = optimoptions('fminunc');
+    elseif method == "fminsearch"
+        options = optimset("fminsearch");
+    end
+end
+
+for(i=1:length(signals))
+    shifted = signals(:,i);
+    parsFit=[0 0];
+    if method == "fminunc"
+        parsFit=fminunc(@func,parsFit,options);
+    else
+        parsFit=fminsearch(@func,parsFit,options);
+    end
+    k = k+1;
+    rslt(1,i) = parsFit(1);
+    rslt(2,i) = round(parsFit(2),5);
+end
+
+end
+function y=func(pars)
+f=pars(1);     %Frequency Shift [Hz]
+p=pars(2);     %Phase Shift [deg]
+global t
+global range
+global fid;
+global shifted
+global visu
+global k
+ffid = fftshift(fft(addphase(fid.*exp(1i*t'*f*2*pi*0.001),p)));
+fshifted = fftshift(fft(shifted));
+corr = dot(ffid(range),fshifted(range));
+if visu == true
+    figure(99)
+    plot(range,ffid(range),range,fshifted(range))
+    drawnow;
+    title(k)
+end
+y= -1 * double((real(corr)));
+end
+%from addphase.m
+% Jamie Near, McGill University 2014. 
+function PhasedSpecs=addphase(specs,AddedPhase);
+PhasedSpecs=specs.*(ones(size(specs))*exp(1i*AddedPhase*pi/180));
+end
